dab0e2f89122b9f67bd8c37bca02caeda2aad3e5
hw <410518072@qq.com>
2024-10-17 22:26:03 +0800

    使用tmgg框架

--- src/main/java/cn/moon/DockerAdminBootApplication.java
+++ src/main/java/cn/moon/DockerAdminBootApplication.java
@@ -1,23 +1,21 @@
 package cn.moon;
 
 
+import io.tmgg.BasePackage;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
-@EnableScheduling
-@ServletComponentScan
 @Slf4j
-public class BootApplication {
+@ComponentScan(basePackageClasses = BasePackage.class)
+public class DockerAdminBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootApplication.class, args);
+        SpringApplication.run(DockerAdminBootApplication.class, args);
     }
 
 }
