aa7a17815a8483c5fd49a00ae47957d68786b1ae
jiangtao <jiangtao.cctv@qq.com>
2025-02-13 15:42:02 +0800

    优化

--- web/src/layouts/index.jsx
+++ web/src/layouts/index.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {Layouts} from "@tmgg/tmgg-system";
+import logo from '../assets/logo.jpg'
 
 export default class extends React.Component {
 
 
   render() {
-    return <Layouts {...this.props}></Layouts>
+    return <Layouts {...this.props} logo={logo}></Layouts>
   }
 
 
