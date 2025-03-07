d2246dc9f7cc00fe77a344df2d3ce51e3c0bf871
jiangtao <jiangtao.cctv@qq.com>
2025-03-07 15:59:04 +0800

    优化

--- web/src/app.jsx
+++ web/src/app.jsx
@@ -1,14 +1,8 @@
 // 调整umi 默认配置
 
-import {initBase, patchClientRoutesRegistered} from "@tmgg/tmgg-base";
-import {initSystem} from "@tmgg/tmgg-system";
+import {initBase} from "@tmgg/tmgg-base";
 
 
 initBase()
-initSystem()
-
-export function patchClientRoutes({ routes }) {
-  patchClientRoutesRegistered(routes)
-}
 
 
