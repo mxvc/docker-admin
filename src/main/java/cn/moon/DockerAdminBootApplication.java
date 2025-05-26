bffc39bc462237481768848cf504ed8f745cc2a4
jiangtao <jiangtao.cctv@qq.com>
2025-05-26 10:44:35 +0800

    升级

--- src/main/java/cn/moon/DockerAdminBootApplication.java
+++ src/main/java/cn/moon/DockerAdminBootApplication.java
@@ -5,6 +5,7 @@ import io.tmgg.BasePackage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableAsync
 @Slf4j
 @ComponentScan(basePackageClasses = {BasePackage.class, DockerAdminBootApplication.class})
+@EntityScan(basePackageClasses =  {BasePackage.class, DockerAdminBootApplication.class})
 public class DockerAdminBootApplication {
 
     public static void main(String[] args) {
