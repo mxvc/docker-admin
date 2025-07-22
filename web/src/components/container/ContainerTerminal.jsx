9775bc197552e4c79b4976ca28ed17c63ed5d977
jiangtao <jiangtao.cctv@qq.com>
2025-07-22 16:09:09 +0800

    终端-fe

---
+++ web/src/components/container/ContainerTerminal.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import {Terminal} from "xterm";
+import "xterm/css/xterm.css"
+import { AttachAddon } from '@xterm/addon-attach';
+
+import { FitAddon } from 'xterm-addon-fit'
+import {SysUtil} from "@tmgg/tmgg-base";
+
+export default class extends React.Component {
+
+
+    cmd = ""
+
+    domRef = React.createRef();
+
+    componentDidMount() {
+        this.initXterm()
+    }
+    initXterm = () => {
+        let term = new Terminal({
+            rendererType: "canvas", //渲染类型
+            rows: 100, //行数
+            convertEol: true, //启用时，光标将设置为下一行的开头
+            disableStdin: false, //是否应禁用输入。
+            cursorStyle: "underline", //光标样式
+            cursorBlink: true, //光标闪烁
+            fontSize:'small',
+            theme: {
+
+                foreground: "#1bd206", //字体
+                background: "#000000", //背景色
+                cursor: "help", //设置光标
+            }
+        })
+
+        const {hostId,containerId} = this.props
+
+        this.webSocket = new WebSocket(this.getWsUrl(hostId, containerId));
+
+        const attachAddon = new AttachAddon(this.webSocket);
+        term.loadAddon(attachAddon);
+        let fitAddon = new FitAddon();
+        term.loadAddon(fitAddon)
+
+        fitAddon.fit()
+
+        term.open(this.domRef.current)
+        term.focus()
+
+
+        this.term = term
+    };
+
+
+    getWsUrl(hostId, containerId) {
+        let url = SysUtil.getServerUrl() + "ws/terminal?hostId=" + hostId + "&containerId=" + containerId;
+
+        url = location.protocol.replace("http","ws") + "//" +  location.host + url
+
+
+        console.log('ws url:', url)
+        return url;
+    }
+
+    componentWillUnmount() {
+        this.term?.dispose()
+        this.term = null
+    }
+    render() {
+        return <div>
+            <div ref={this.domRef} ></div>
+
+        </div>
+    }
+}
--- web/src/pages/host/containerView.jsx
+++ web/src/pages/host/containerView.jsx
@@ -3,6 +3,7 @@ import {Card, Tabs} from "antd";
 import ContainerLog from "../../components/container/ContainerLog";
 import ContainerFile from "../../components/container/ContainerFile";
 import {PageUtil} from "@tmgg/tmgg-base";
+import ContainerTerminal from "../../components/container/ContainerTerminal";
 
 
 export default class extends React.Component {
@@ -11,17 +12,20 @@ export default class extends React.Component {
     const {containerId, hostId} = PageUtil.currentLocationQuery();
 
     return <Card style={{minHeight:'calc(100vh - 100px)'}}>
-      <Tabs  destroyInactiveTabPane >
+      <Tabs  destroyOnHidden >
         <Tabs.TabPane tab="日志" key="container-log">
           <ContainerLog hostId={hostId} containerId={containerId}/>
         </Tabs.TabPane>
+
 
 
         <Tabs.TabPane tab="文件" key="container-file">
           <ContainerFile hostId={hostId} containerId={containerId}/>
         </Tabs.TabPane>
 
-
+        <Tabs.TabPane tab="终端 (BETA)" key="container-term">
+          <ContainerTerminal hostId={hostId} containerId={containerId}/>
+        </Tabs.TabPane>
 
       </Tabs>
     </Card>
