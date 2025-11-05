81204cf91364defbe3e26acb2a5c98c84aa3d356
jiangtao <jiangtao.cctv@qq.com>
2025-11-05 14:39:50 +0800

    升级2.0

--- src/main/java/io/github/mxvc/docker/admin/entity/AppGroup.java
+++ src/main/java/io/github/mxvc/docker/admin/entity/AppGroup.java
@@ -1,8 +1,25 @@
 package io.github.mxvc.docker.admin.entity;
 
 import io.tmgg.data.domain.BaseEntity;
+import io.tmgg.lang.ann.Remark;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldNameConstants;
 
 
+@Remark("应用分租")
+@Entity
+@Getter
+@Setter
+@FieldNameConstants
 public class AppGroup extends BaseEntity {
+
+    @Remark("名称")
+    private String name;
+
+
+    @Remark("排序")
+    private Integer seq;
 
 }
