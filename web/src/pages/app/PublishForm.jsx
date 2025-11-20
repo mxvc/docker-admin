afdec62241ddf0e6ce12dd13aaafd31bf3d966cb
jiangtao <jiangtao.cctv@qq.com>
2025-11-20 17:05:54 +0800

    init

--- web/src/pages/app/PublishForm.jsx
+++ web/src/pages/app/PublishForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Button, Collapse, Divider, Form, Input, message, Modal, Skeleton, Switch, Typography} from "antd";
+import {Button, Card, Collapse, Divider, Form, Input, message, Modal, Skeleton, Switch, Typography} from "antd";
 import {FieldSelect, Gap, HttpUtil, PageUtil} from "@jiangood/springboot-admin-starter";
 
 /**
@@ -64,32 +64,37 @@ export default class extends React.Component {
 
 
         return <>
-            <Typography.Title level={5}>手动发布</Typography.Title>
-            <Form onFinish={this.updateVersion} layout={'inline'}>
-                <Form.Item name='imageTag' rules={[{required: true}]}>
-                    <Input style={{width: 150}} placeholder='请输入版本号'/>
-                </Form.Item>
-                <Form.Item label=' '>
-                    <Button type="primary" danger htmlType='submit'>更新应用</Button>
-                </Form.Item>
-            </Form>
+            <Card title='手动发布'>
+                <Form onFinish={this.updateVersion} layout={'inline'}>
+                    <Form.Item name='imageTag' rules={[{required: true}]}>
+                        <Input style={{width: 150}} placeholder='请输入版本号'/>
+                    </Form.Item>
+                    <Form.Item label=' '>
+                        <Button type="primary" danger htmlType='submit'>更新应用</Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+
 
             <Divider />
 
-            <Typography.Title level={5}>自动发布</Typography.Title>
-            <Form onValuesChange={changedValues => this.setAutoDeploy(changedValues.autoDeploy)}>
-                <Form.Item
-                    name='autoDeploy'
-                    valuePropName="checked"
-                    initialValue={app.autoDeploy}>
-                    <Switch/>
-                </Form.Item>
-            </Form>
-            <Typography.Text  italic >项目构建成功后，自动更新</Typography.Text>
+            <Card title='自动发布'>
+                <Form onValuesChange={changedValues => this.setAutoDeploy(changedValues.autoDeploy)}>
+                    <Form.Item
+                        name='autoDeploy'
+                        valuePropName="checked"
+                        initialValue={app.autoDeploy}>
+                        <Switch/>
+                    </Form.Item>
+                </Form>
+                项目构建成功后，自动更新
+            </Card>
+
 
             <Divider  ></Divider>
-            <Typography.Title level={5}>复制应用</Typography.Title>
+            <Typography.Title level={5}></Typography.Title>
 
+            <Card title='复制应用'>
             <Form onFinish={this.copyApp} layout='inline'>
                 <Form.Item name='hostId' rules={[{required: true}]}>
                     <FieldSelect url='admin/host/options' placeholder='请选择新主机' style={{width: 300}}/>
@@ -97,8 +102,9 @@ export default class extends React.Component {
 
                 <Button type="primary" danger htmlType='submit'>确定复制</Button>
             </Form>
-            <Typography.Text
-                italic>注意：复制应用不会自动部署，也不会复制主机上的文件</Typography.Text>
+                <Gap />
+                注意：复制应用不会自动部署，也不会复制主机上的文件
+            </Card>
 
         </>
     }
